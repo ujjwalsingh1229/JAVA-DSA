8e65e18ccfebdc086c9ca8610dbc64afe0c5840b
UJJWALe <thakurujjwal1221@gmail.com>
2025-10-09 19:02:09 +0530

    7:00

--- ARRAY/SORTING/Bubblesort.java
+++ ARRAY/SORTING/Bubblesort.java
@@ -7,7 +7,7 @@ public class Bubblesort{
         for(int turn=0; turn<arr.length-1;turn++){
             for(int j=0;j<arr.length-1-turn;j++){
                 if(arr[j]>arr[j+1]){
-                    //Swap
+                    //Swap}
                     int temp =arr[j];
                     arr[j]=arr[j+1];
                     arr[j+1]=temp;
