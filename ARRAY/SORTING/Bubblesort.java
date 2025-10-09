1ea9ff6abad84bd6737a73842d9221442b4ef74a
UJJWALe <thakurujjwal1221@gmail.com>
2025-10-09 18:51:59 +0530

    6:51

---
+++ ARRAY/SORTING/Bubblesort.java
@@ -0,0 +1,34 @@
+package ARRAY.SORTING;
+
+
+// Bubble sort 
+public class Bubblesort{
+    public static void bubblesort(int arr[]){
+        for(int turn=0; turn<arr.length-1;turn++){
+            for(int j=0;j<arr.length-1-turn;j++){
+                if(arr[j]>arr[j+1]){
+                    //Swap
+                    int temp =arr[j];
+                    arr[j]=arr[j+1];
+                    arr[j+1]=temp;
+                }
+            }
+        }
+
+    }
+    public static void printarr(int arr[]){
+        for(int i=0;i<arr.length;i++){
+            System.out.print(arr[i]+" ");
+        }
+        System.out.println();
+        
+    }
+    public static void main(String awrgs[]){
+        int arr[]={5,4,1,3,2};
+        bubblesort(arr);
+        printarr(arr);
+
+    }
+    
+    
+}
